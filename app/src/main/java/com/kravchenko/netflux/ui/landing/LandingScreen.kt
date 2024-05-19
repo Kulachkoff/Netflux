05814eed3b0813085741dc7cc6fdedf5061e4ace
Maksym Kravchenko <caawabungaa@gmail.com>
2024-05-20 00:08:45 +0300

    LandingScreen clean-up

--- app/src/main/java/com/kravchenko/netflux/ui/landing/LandingScreen.kt
+++ app/src/main/java/com/kravchenko/netflux/ui/landing/LandingScreen.kt
@@ -39,85 +39,111 @@ fun LandingScreen(navController: NavController, viewModel: LandingViewModel) {
         }
         is LandingState.Success -> {
             val posters = (landingState as LandingState.Success).posters
-            ImageCarousel(
-                modifier = Modifier.fillMaxSize(),
+            LandingContent(
+                navController = navController,
                 posters = posters
             )
-            Column(
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Spacer(modifier = Modifier.weight(1f))
-                Card(
-                    shape = RoundedCornerShape(topStart = 4.dp, topEnd = 4.dp),
-                    colors = CardDefaults.cardColors(PrimaryBlack90),
-                    modifier = Modifier.fillMaxWidth(),
-                ) {
-                    Column(
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        modifier = Modifier.padding(start = 25.dp, top = 30.dp, end = 25.dp, bottom = 50.dp)
-                    ) {
-                        Text(
-                            modifier = Modifier,
-                            text = "Watch movies anytime anywhere",
-                            color = Color.White,
-                            fontSize = 20.sp,
-                            fontWeight = FontWeight.Bold,
-                            textAlign = TextAlign.Center,
-                            fontFamily = inter,
-                        )
-                        Text(
-                            text = "Explore a vast collection of blockbuster movies, timeless classics, and the latest releases.",
-                            color = Color.White,
-                            fontSize = 13.sp,
-                            modifier = Modifier.padding(top = 10.dp),
-                            textAlign = TextAlign.Center,
-                            fontWeight = FontWeight.SemiBold,
-                            fontFamily = inter
-                        )
-                        Button(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(top = 20.dp),
-                            onClick = { navController.navigate(Routes.Login.route) },
-                            shape = RoundedCornerShape(4.dp),
-                            colors = ButtonDefaults.buttonColors(PrimaryGreen)
-                        ) {
-                            Text(
-                                modifier = Modifier.padding(vertical = 10.dp),
-                                text = "Login",
-                                color = Color.Black,
-                                fontFamily = inter,
-                                fontSize = 14.sp,
-                                fontWeight = FontWeight.Bold
-                            )
-                        }
-                        Button(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(top = 20.dp),
-                            onClick = { navController.navigate(Routes.Signup.route) },
-                            shape = RoundedCornerShape(4.dp),
-                            border = BorderStroke(2.dp, PrimaryGreen),
-                            colors = ButtonDefaults.outlinedButtonColors(Color.Transparent)
-                        ) {
-                            Text(
-                                modifier = Modifier.padding(vertical = 10.dp),
-                                text = "Sign Up",
-                                color = PrimaryGreen,
-                                fontFamily = inter,
-                                fontSize = 14.sp,
-                                fontWeight = FontWeight.Bold
-                            )
-                        }
-                    }
-                }
-            }
         }
         is LandingState.Error -> {
             val message = (landingState as LandingState.Error).message
-            Text(text = message, color = Color.Red)
+            Text(
+                text = message,
+                color = Color.Red
+            )
         }
+    }
+}
+
+@Composable
+fun LandingContent(
+    navController: NavController,
+    posters: List<Drawable>
+) {
+    ImageCarousel(
+        modifier = Modifier.fillMaxSize(),
+        posters = posters
+    )
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Spacer(modifier = Modifier.weight(1f))
+        AuthorizationSelection(navController = navController)
+    }
+}
+
+@Composable
+fun AuthorizationSelection(navController: NavController) {
+    Card(
+        shape = RoundedCornerShape(topStart = 4.dp, topEnd = 4.dp),
+        colors = CardDefaults.cardColors(PrimaryBlack90),
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(start = 25.dp, top = 30.dp, end = 25.dp, bottom = 50.dp)
+        ) {
+            Text(
+                modifier = Modifier,
+                text = "Watch movies anytime anywhere",
+                color = Color.White,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                fontFamily = inter,
+            )
+            Text(
+                text = "Explore a vast collection of blockbuster movies, timeless classics, and the latest releases.",
+                color = Color.White,
+                fontSize = 13.sp,
+                modifier = Modifier.padding(top = 10.dp),
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.SemiBold,
+                fontFamily = inter
+            )
+            AuthorizationButton(
+                text = "Login",
+                textColor = Color.Black,
+                colors = ButtonDefaults.buttonColors(PrimaryGreen),
+                onClick = { navController.navigate(Routes.Login.route) }
+            )
+            AuthorizationButton(
+                text = "Sign Up",
+                textColor = PrimaryGreen,
+                colors = ButtonDefaults.outlinedButtonColors(Color.Transparent),
+                onClick = { navController.navigate(Routes.Signup.route) },
+                borderStroke = BorderStroke(2.dp, PrimaryGreen)
+            )
+        }
+    }
+}
+
+@Composable
+fun AuthorizationButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    colors: ButtonColors,
+    borderStroke: BorderStroke? = null,
+    textColor: Color,
+    onClick: () -> Unit
+) {
+    Button(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 20.dp),
+        shape = RoundedCornerShape(4.dp),
+        colors = colors,
+        onClick = onClick,
+        border = borderStroke
+    ) {
+        Text(
+            modifier = Modifier.padding(vertical = 10.dp),
+            text = text,
+            color = textColor,
+            fontFamily = inter,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
 
